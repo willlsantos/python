e44fb1dffb67c2465a2bea6eddd16b65c7d3201f
Willian dos Santos <will.santos92@gmail.com>
2023-03-25 14:15:38 -0300

    reorganizando repositorio

--- script1.py
+++
@@ -1,5 +0,0 @@
-# Script que leia o nome de uma pessoa e mostre uma mensagem de boas vindas de acordo com o valor digitado
-nome = input('Olá, Qual o seu nome?')
-
-print('Olá ' + nome + '!. Seja bem-vindo ao script.')
-
--- script2.py
+++
@@ -1,9 +0,0 @@
-# Criar script que leia o dia, o mes e o ano de nascimento de uma pessoa e mostre uma mensagem com data formatada
-
-dia = input("Qual o dia que você nasceu? ")
-mes = input("Qual o mês que você nasceu? ")
-ano = input("Qual o ano que você nasceu? ")
-
-data = ("Você nasceu em: " + dia + "/" + mes + "/" + ano + ". Correto? ")
-
-print (data)
