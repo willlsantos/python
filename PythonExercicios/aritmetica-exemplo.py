c8776e325213803e47c21e59c7bfd4e3c5cba874
Willian dos Santos <will.santos92@gmail.com>
2023-04-16 17:20:46 -0300

    aula 7 - aritméticos

---
+++ PythonExercicios/aritmetica-exemplo.py
@@ -0,0 +1,18 @@
+numero_1 = 5
+numero_2 = 2
+
+soma = numero_1 + numero_2
+subtracao = numero_1 - numero_2
+multiplicacao = numero_1 * numero_2
+divisao = numero_1 / numero_2
+divisao_inteira = numero_1 // numero_2
+modulo = numero_1 % numero_2
+exponenciacao = numero_1 ** numero_2
+
+print(soma) # 7
+print(subtracao) # 3
+print(multiplicacao)  # 10
+print(divisao) # 2.5
+print(divisao_inteira) # 2
+print(modulo)  # 1
+print(exponenciacao) # 25
