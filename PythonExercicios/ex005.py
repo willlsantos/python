853223bccb12e1e834a133a0650f9a42a6bce3e2
Willian dos Santos <will.santos92@gmail.com>
2023-04-16 18:42:23 -0300

    exercicio 5

--- PythonExercicios/ex005.py
+++ PythonExercicios/ex005.py
@@ -1,1 +1,8 @@
-# TODO Faça um programa que leia um núymero inteiro e mostre na tela o seu sucessor e seu antecessor
+# TODO Faça um programa que leia um número inteiro e mostre na tela o seu sucessor e seu antecessor
+
+n1 = int(input('Digite um número: '))
+
+ante = n1 - 1
+suc = n1 + 1
+
+print('O Antecessor é {}, \no número digitado é o {}, \ne o Sucessor é o {}.'.format(ante, n1, suc))
