72a8070fc81dbecfc581c5e129f0e0f894117635
Willian dos Santos <will.santos92@gmail.com>
2023-04-16 18:47:11 -0300

    resolvendo ex006

--- PythonExercicios/ex006.py
+++ PythonExercicios/ex006.py
@@ -1,1 +1,9 @@
 # Todo crie um programa que leia um número e mostre o seu dobro, triplo e raiz quadrada
+
+n1 = int(input('Digite um número: '))
+
+dobro = n1 * 2
+triplo = n1 * 3
+raiz = n1 ** (1/2)
+
+print('O Dobro é {}, \nO Triplo é {}, \nRaiz Quadrada é {}'.format(dobro, triplo, raiz))
