eea2401f35069198feda0ddceba9d0da19be0d83
Willian dos Santos <will.santos92@gmail.com>
2023-04-16 11:36:08 -0300

    Resolvendo desafio da aula 6, exercicio 4

--- PythonExercicios/ex004c.py
+++ PythonExercicios/ex004c.py
@@ -1,17 +1,17 @@
-#Leia dois números e tente mostrar a soma entre eles
+#Leia o tipo do que foi digitado e coloque na tela o que é
 
 v1 = input("Digite algo: ")
 
+# Python 2 ou anterior
+print('O tipo primitivo é ', type(v1))
 
-print(v1.isalnum())
+print('É um Valor Alfanumérico? ',v1.isalnum())
+print('É um Valor Alfabético? ',v1.isalpha())
+print('É um Valor Númerico? ',v1.isnumeric())
+print('É um Valor em Minúsculo? ',v1.islower())
+print('É um Valor em Maiúsculo? ',v1.isupper())
+print('Só tem espaços? ', v1.isspace())
+print('Está capitalizada? ', v1.istitle())
+#print('É um Valor que possui alguma Valor em Maiúsculo?',v1.is())
 
-if v1.isalnum():
-    print('O que você digitou é {} e é Alfanumérico'.format(v1))
-elif v1.isalpha():
-    print('O que você digitou é {} e é Alfabético'.format(v1))
-elif v1.isnumeric():
-    print('O que você digitou é {} e é Numérico'.format(v1))
-elif v1.islower():
-    print('O que você digitou é {} e está com letras minúsculas'.format(v1))
-elif v1.isupper():
-    print('O que você digitou é {} e está com letras maiúsculas'.format(v1))
+# TODO Python 3
