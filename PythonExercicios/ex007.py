3ce57ac4e46da11e3d8a7282b050637d9cad376a
Willian dos Santos <will.santos92@gmail.com>
2023-04-17 06:56:54 -0300

    Resolvendo exercicio 007

--- PythonExercicios/ex007.py
+++ PythonExercicios/ex007.py
@@ -1,3 +1,17 @@
 #Todo Desenvolva um programa que leia as duas notas de um aluno, calcule e mostre sua média
 
-##Cuidado como calcular, se ligue na ordem de precedencia.
+##Cuidado como calcular, se ligue na ordem de precedencia
+
+n1 = int(input('Digite a primeira nota:'))
+n2 = int(input('Digite a segunda nota: '))
+
+soma = n1 + n2
+
+result = soma / 2
+
+#Primeira forma de Print
+
+#print('\nA média das suas notas é {}'.format((n1+n2)/2))
+
+# Segunda forma de Print
+print('\nA média das suas notas é {}'.format(result)
