4bbd5f25f32c5661adc46398268fa1fa29e548d5
Willian dos Santos <will.santos92@gmail.com>
2023-04-17 17:37:48 -0300

    adicionando correção

--- PythonExercicios/ex005.py
+++ PythonExercicios/ex005.py
@@ -6,3 +6,10 @@ ante = n1 - 1
 suc = n1 + 1
 
 print('O Antecessor é {}, \no número digitado é o {}, \ne o Sucessor é o {}.'.format(ante, n1, suc))
+
+## Correção Guanabara outra forma. Quanto menos variaveis, menos memória seu programa consome.
+'''
+n = int(input('Digite um número: '))
+print('Analisando o valor {}, sei antecessor é {} e o sucessor é {}'.format(n, (n-1), (n+1)))
+
+'''
