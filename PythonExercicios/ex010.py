06c80800967814ca89784ec709a168170c89c7d2
Willian dos Santos <will.santos92@gmail.com>
2023-04-17 07:17:40 -0300

    ex010 resolvido

--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,2 +1,9 @@
 #TODO Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
 # considere U$ 1,00 = 3,27
+
+din = int(input('Quanto de dinheiro você tem na carteira?: '))
+
+preco_dol = 3.27
+compra_dol = din / preco_dol
+
+print('Com R${}, você pode comprar U${:.2f} Dólares'.format(din, compra_dol))
