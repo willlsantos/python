ad256a2210b589a7d987c24d3dc718a2252f5ec2
Willian dos Santos <will.santos92@gmail.com>
2023-04-17 07:45:40 -0300

    ex013 resolvido

--- PythonExercicios/ex013.py
+++ PythonExercicios/ex013.py
@@ -1,1 +1,8 @@
-#TODO faça um algoritmo que leia o salário de um funcionário, e mostre seu novo salário com 15% de aumento
+# TODO faça um algoritmo que leia o salário de um funcionário, e mostre seu novo salário com 15% de aument
+
+sal_atual = float(input('Digite o seu salário: '))
+
+aum = 0.15
+novo_sal = str(round(sal_atual + (sal_atual * aum), 2)).replace('.', ',')
+
+print('O seu novo salário é de R${}'.format(novo_sal))
