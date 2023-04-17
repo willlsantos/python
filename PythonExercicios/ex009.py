d06b320c0bcedbb8d765717658869b642aa450e3
Willian dos Santos <will.santos92@gmail.com>
2023-04-17 07:10:10 -0300

    ex009 resolvido

--- PythonExercicios/ex009.py
+++ PythonExercicios/ex009.py
@@ -1,1 +1,15 @@
 #TODO faça um programa que leia um número inteiro qualquer e mostre na tela a sua tabuada.
+# Tabuada Simples
+
+n1 = int(input('Tabuada de qual número você quer que seja exibida?: '))
+
+print('{} X 1 = {}'.format(n1, n1*1))
+print('{} X 2 = {}'.format(n1, n1*2))
+print('{} X 3 = {}'.format(n1, n1*3))
+print('{} X 4 = {}'.format(n1, n1*4))
+print('{} X 5 = {}'.format(n1, n1*5))
+print('{} X 6 = {}'.format(n1, n1*6))
+print('{} X 7 = {}'.format(n1, n1*7))
+print('{} X 8 = {}'.format(n1, n1*8))
+print('{} X 9 = {}'.format(n1, n1*9))
+print('{} X 10 = {}'.format(n1, n1*10))
