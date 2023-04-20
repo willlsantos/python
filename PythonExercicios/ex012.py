0c2d4ed003b353404fa75135eeeffa2cc67a0dc8
Willian dos Santos <will.santos92@gmail.com>
2023-04-20 07:08:44 -0300

    pequenos ajustes

--- PythonExercicios/ex012.py
+++ PythonExercicios/ex012.py
@@ -1,6 +1,6 @@
 #TODO faça um algoritmo que leia o preço de um produto e mostre seu novo preço, com 5% de desconto
 
-prod = float(input('Qual é o preço do produto?: '))
+prod = float(input('Qual é o preço do produto? R$ '))
 desc = 0.05
 
 novo_valor = prod - (prod * desc)
