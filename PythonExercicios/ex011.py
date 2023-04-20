6acd804d3e448487e7d392a2b0df994fcfd8ff06
Willian dos Santos <will.santos92@gmail.com>
2023-04-20 06:38:31 -0300

    pequenos ajustes ex011

--- PythonExercicios/ex011.py
+++ PythonExercicios/ex011.py
@@ -1,8 +1,8 @@
 # TODO faça um programa que leia a largura e a altura de uma parede em metros, calcule a sua área
 #  e a  quantidade de tinta necessária para pintá-la, sabendo que cada litro de tinta pinta uma area de 2m²
 
-larg = int(input('Digite a Largura da parede: '))
-alt = int(input('Digite a Altura da Parede: '))
+larg = float(input('Digite a Largura da parede: '))
+alt = float(input('Digite a Altura da Parede: '))
 
 a = larg * alt
 
@@ -10,4 +10,4 @@ l = 2
 
 result = a / l
 
-print ('Você precisa de {} latas de tintas para pintar essa parede'.format(result))
+print ('Sua parede tem {}m²\nVocê precisa de {:.2} latas de tintas para pintar essa parede'.format(a, result))
