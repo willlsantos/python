ee61c402dee58f2a246af5cc31ba34cfaa37b0c8
Willian dos Santos <will.santos92@gmail.com>
2023-04-20 06:19:37 -0300

    ajuste da variavel dol

--- PythonExercicios/ex010.py
+++ PythonExercicios/ex010.py
@@ -1,7 +1,7 @@
 #TODO Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar
 # considere U$ 1,00 = 3,27
 
-din = int(input('Quanto de dinheiro você tem na carteira?: '))
+din = float(input('Quanto de dinheiro você tem na carteira? R$: '))
 
 preco_dol = 3.27
 compra_dol = din / preco_dol
