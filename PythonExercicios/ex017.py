a0dbd19dd16384c4e94c0aec5879559553c136c8
Willian dos Santos <will.santos92@gmail.com>
2023-04-23 09:16:22 -0300

    exercicio 17 resolvido

--- PythonExercicios/ex017.py
+++ PythonExercicios/ex017.py
@@ -1,2 +1,10 @@
 # TODO Faça um programa que leia o comprimento do cateto opsto e do cateto adjacente de um triangulo retangulo
 # calcule e mostre o comprimento da hipotenusa.
+from math import hypot
+
+co = float(input('Digite o valor do Cateto Oposto: '))
+ca = float(input('Digite o valor do Cateto Adjacente: '))
+
+h =  hypot(co, ca)
+
+print('Se o cateto opsoto é {} e o cateto adjacente é {}\nEntão a hipotenusa é {:.2f}'.format(co, ca, h))
