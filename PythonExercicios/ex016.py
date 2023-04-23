c214a9b65b3f915255659c7f36b930c7bd67462a
Willian dos Santos <will.santos92@gmail.com>
2023-04-23 08:15:08 -0300

    exercicio resolvido

--- PythonExercicios/ex016.py
+++ PythonExercicios/ex016.py
@@ -1,2 +1,9 @@
-# Crie um programa que leia um número Real e mostre na tela a sua porção inteira. Ex: 6.127 part inteira é 6
+# TODO Crie um programa que leia um número Real e mostre na tela a sua porção inteira. Ex: 6.127 part inteira é 6
 # Dentro do módulo math tem a função que faz isso
+
+from math import modf
+
+n1 = float(input('Digite um número: '))
+por = modf(n1)
+
+print('A Parte inteira do número {} é {}'.format(n1, por))
