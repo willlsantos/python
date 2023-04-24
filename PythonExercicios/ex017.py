1993c167743b91c207f1a876718c1b8c4f45b088
Willian dos Santos <will.santos92@gmail.com>
2023-04-24 08:02:35 -0300

    ajuste espaço

--- PythonExercicios/ex017.py
+++ PythonExercicios/ex017.py
@@ -5,6 +5,6 @@ from math import hypot
 co = float(input('Digite o valor do Cateto Oposto: '))
 ca = float(input('Digite o valor do Cateto Adjacente: '))
 
-h =  hypot(co, ca)
+h = hypot(co, ca)
 
 print('Se o cateto opsoto é {} e o cateto adjacente é {}\nEntão a hipotenusa é {:.2f}'.format(co, ca, h))
