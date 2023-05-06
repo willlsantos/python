ef09b3881fcc7090da49a66d096ae20f3a7c3c38
Willian dos Santos <will.santos92@gmail.com>
2023-05-06 12:21:41 -0300

    ex021 resolvido

--- PythonExercicios/ex021.py
+++ PythonExercicios/ex021.py
@@ -1,4 +1,4 @@
 #TODO faça um programa em python que abra e reproduza o audio de arquivo mp3.
 import webbrowser
 
-s =  webbrowser.open("/home/will/Música/01. You Da One.mp3")
+s = webbrowser.open("/home/will/Música/01. You Da One.mp3")
