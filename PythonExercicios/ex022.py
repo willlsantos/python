c1677a82a751515180e4c0d1b325e08de374f83b
Willian dos Santos <will.santos92@gmail.com>
2023-05-06 13:19:15 -0300

    Aula 9 - Manipulando Texto

---
+++ PythonExercicios/ex022.py
@@ -0,0 +1,6 @@
+'''TODO Um programa que leia o nome completo e exiba:
+ O nome com todas as letras maiusculas
+ o nome com todas as letras minusculas
+ Quantas letras ao todo (sem considerar espaço);
+ Quantas letras tem o primeiro nome
+'''
